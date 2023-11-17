90614fc9f175b95da61b591ea88171b7f3956295
AleksAagesen <aleksanderha2907@gmail.com>
2023-11-17 14:19:10 +0100

    p3 changes

--- ex3.py
+++ ex3.py
@@ -1,16 +1,15 @@
 dict ={}
-istrue = True
-while istrue:
-    user = input('Type a input in this format: key;value ')
-    if user == 'revert':
-        istrue = False
+user_input = None
+while user_input != 'revert':
+    user_input = input('Type a input in this format: key;value ')
+    if user_input == 'revert':
+        for key_val in dict.items():
+            print(key_val)
+
+        dict2 = {value: key for key, value in dict.items()}
+        for key_val in dict2.items():
+            print(key_val)
     else:
-        key, value = user.split(';')
+        key, value = user_input.split(';')
         dict[key] = value
 
-for key_val in dict.items():
-    print(key_val)
-
-dict2 = {value: key for key, value in dict.items()}
-for key_val in dict2.items():
-    print(key_val)
